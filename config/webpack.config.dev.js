c4fa5d07eba5767083bcd108ea3b94ba4ac04c2f
Trim21 <i@trim21.me>
2020-05-22 02:24:48 +0800

    fix(build): dev script metadata requre file name
    close #47

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,44 +1,43 @@
-const path = require('path')
-const merge = require('webpack-merge')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const LiveReloadPlugin = require('webpack-livereload-plugin')
-const UserScriptMetaDataPlugin = require('userscript-metadata-webpack-plugin')
-const metadata = require('./metadata')
+const path = require("path");
+const merge = require("webpack-merge");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const LiveReloadPlugin = require("webpack-livereload-plugin");
+const UserScriptMetaDataPlugin = require("userscript-metadata-webpack-plugin");
+const metadata = require("./metadata");
 
-const webpackConfig = require('./webpack.config.base')
+const webpackConfig = require("./webpack.config.base");
 
-const output = {
-  filename: metadata.name + '.prod.user.js'
-}
-
-metadata.require.push('file://' + path.resolve(__dirname, '../dist', output.filename))
+metadata.require.push(
+  "file://" + path.resolve(__dirname, "../dist/index.prod.user.js")
+);
 
 const cfg = merge(webpackConfig, {
   entry: {
     prod: webpackConfig.entry,
-    dev: path.resolve(__dirname, './empty.js'),
+    dev: path.resolve(__dirname, "./empty.js"),
   },
   output: {
-    filename: 'index.[name].user.js',
-    path: path.resolve(__dirname, '../dist'),
+    filename: "index.[name].user.js",
+    path: path.resolve(__dirname, "../dist"),
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   watch: true,
   watchOptions: {
-    ignored: /node_modules/
+    ignored: /node_modules/,
   },
   plugins: [
     new LiveReloadPlugin({
       delay: 500,
     }),
     new UserScriptMetaDataPlugin({
-      metadata
-    })
-  ]
-})
+      metadata,
+    }),
+  ],
+});
 
 if (process.env.npm_config_report) {
-  cfg.plugins.push(new BundleAnalyzerPlugin())
+  cfg.plugins.push(new BundleAnalyzerPlugin());
 }
 
-module.exports = cfg
+module.exports = cfg;
