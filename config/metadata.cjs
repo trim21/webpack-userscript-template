e9a936eb75a19f5a8b3679a1035f9f76e7828759
Trim21 <i@trim21.me>
2021-04-02 09:42:00 +0800

    update webpack config

--- config/metadata.cjs
+++ config/metadata.cjs
@@ -1,19 +1,19 @@
-const pkg = require('../package.json')
+const { author, dependencies, repository, version } = require('../package.json')
 
 module.exports = {
   name: 'webpack-userscript-template',
   namespace: 'https://trim21.me/',
-  version: pkg.version,
-  author: pkg.author,
-  source: pkg.repository.url,
+  version: version,
+  author: author,
+  source: repository.url,
   // 'license': 'MIT',
   match: [
     'http://www.example.com/*'
   ],
   require: [
-    `https://cdn.jsdelivr.net/npm/jquery@${pkg.dependencies.jquery}/dist/jquery.min.js`,
-    `https://cdn.jsdelivr.net/npm/axios@${pkg.dependencies.axios}/dist/axios.min.js`,
-    `https://cdn.jsdelivr.net/npm/axios-userscript-adapter@${pkg.dependencies['axios-userscript-adapter']}/dist/axiosGmxhrAdapter.min.js`,
+    `https://cdn.jsdelivr.net/npm/jquery@${dependencies.jquery}/dist/jquery.min.js`,
+    `https://cdn.jsdelivr.net/npm/axios@${dependencies.axios}/dist/axios.min.js`,
+    `https://cdn.jsdelivr.net/npm/axios-userscript-adapter@${dependencies['axios-userscript-adapter']}/dist/axiosGmxhrAdapter.min.js`,
   ],
   grant: [
     'GM.xmlHttpRequest'
