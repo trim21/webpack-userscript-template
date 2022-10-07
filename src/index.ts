0ddd3c980d2f6fcd66b334928e1a82c1ff3ec8ff
Trim21 <trim21.me@gmail.com>
2022-10-07 20:11:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import "./style/main.less";
 
-import { get } from "./utils";
 //checkout homepage https://github.com/Trim21/gm-fetch for @trim21/gm-fetch
 import GM_fetch from "@trim21/gm-fetch";
 
@@ -8,7 +7,6 @@ async function main() {
   console.log("script start");
 
   // cross domain requests
-  console.log(`uuid: ${await axiosExample()}`);
   console.log(`uuid: ${await fetchExample()}`);
 }
 
@@ -16,13 +14,6 @@ async function fetchExample(): Promise<string> {
   const res = await GM_fetch("https://httpbin.org/uuid");
   const data = await res.json();
   return data.uuid;
-}
-
-async function axiosExample(): Promise<string> {
-  const res = await get<{ uuid: string }>("/uuid", {
-    baseURL: "https://httpbin.org",
-  });
-  return res.data.uuid;
 }
 
 main().catch((e) => {
